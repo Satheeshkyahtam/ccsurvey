49f52ddb5fc2606b372d16aa4406a3423a92fbca
Atul33 <atulbhanushali93@gmail.com>
2021-09-07 21:23:47 +0530

    Minor changes

--- surveys/src/main/java/com/godrej/surveys/dao/ProjectDao.java
+++ surveys/src/main/java/com/godrej/surveys/dao/ProjectDao.java
@@ -50,7 +50,7 @@ public interface ProjectDao {
 	@Select("Select name,sfid,propstrength__project_code__c as segmentCode, region__c as region  FROM " + 
 			//"salesforce.propstrength__projects__c WHERE sfid not in ( 'a1l6F000008DnniQAC','a1l6F000008fqcuQAA','a1l6F000002dTpoQAE') "
 			"salesforce.propstrength__projects__c WHERE sfid not in ('a1l6F000008fqcuQAA','a1l6F000002dTpoQAE') "
-			//+ " and Baseline_Survey_Start_Date__c::date < now()::date and Baseline_Survey_Start_Date__c is not null "
+			+ " and Baseline_Survey_Start_Date__c::date < now()::date and Baseline_Survey_Start_Date__c is not null "
 			+ " ORDER BY name ASC")
 	public List<ProjectDto> getBLProjects(String region);
 	/* END Added by A */
--- surveys/src/main/resources/static/js/common/constants.js
+++ surveys/src/main/resources/static/js/common/constants.js
@@ -1,4 +1,4 @@
 var ctx = '/ccsurveys';
-
+//var ctx = '/ccsurveysuat';
 
 /* var ctx = '';*/
