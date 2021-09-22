19c5a17a68662800400cc7499bac165bb8470f07
Atul33 <atulbhanushali93@gmail.com>
2021-09-22 14:38:29 +0530

    > Added order by booking date in  baseline Query

--- surveys/src/main/java/com/godrej/surveys/baseline/sqlprovider/BaselineSurveySQLProvider.java
+++ surveys/src/main/java/com/godrej/surveys/baseline/sqlprovider/BaselineSurveySQLProvider.java
@@ -77,7 +77,11 @@ public class BaselineSurveySQLProvider {
 			
 			
 			.append(" AND a.propstrength__applicant_email__c is not null ")
-			.append(" AND a.Baseline_Survey_Sent__c <> true ");
+			.append(" AND a.Baseline_Survey_Sent__c <> true ")
+		 
+			.append(" ORDER BY A.propstrength__booking_date__c ");
+		 
+		 
 		return whereClause.toString();
 	}
 
